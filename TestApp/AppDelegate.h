79617ea14a9888427359f8faeb18a2189c4ba956
Dmytro Naumov <Dmytro.Naumov@brainloop.com>
2015-12-18 09:20:42 +0100

    Initial commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Dmytro Naumov on 18/12/15.
+//  Copyright © 2015 Brainloop AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Dmytro Naumov on 18/12/15.
+//  Copyright © 2015 Brainloop AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
